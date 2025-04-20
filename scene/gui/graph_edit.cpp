92938f4f5036f178998ba99b1f8046fcaf8e6932
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-04-20 20:30:34 +0300

    Fix `FOCUS_ACCESSIBILITY` grabbing focus when it is not supposed to, forward `GraphNode` key input to `GraphEdit`.

--- scene/gui/graph_edit.cpp
+++ scene/gui/graph_edit.cpp
@@ -2261,6 +2261,10 @@ void GraphEdit::gui_input(const Ref<InputEvent> &p_ev) {
 		}
 	}
 
+	key_input(p_ev);
+}
+
+void GraphEdit::key_input(const Ref<InputEvent> &p_ev) {
 	if (p_ev->is_pressed()) {
 		if (p_ev->is_action("ui_graph_duplicate", true)) {
 			emit_signal(SNAME("duplicate_nodes_request"));
