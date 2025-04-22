83fea6ca5e389c9ee7fc7ef2bfcc352378bf6a3b
IntrinsicViolet <187171850+IntrinsicViolet@users.noreply.github.com>
2025-04-22 13:18:45 -0500

    approximate comparison
    Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>

--- servers/rendering/renderer_rd/storage_rd/material_storage.cpp
+++ servers/rendering/renderer_rd/storage_rd/material_storage.cpp
@@ -2419,7 +2419,7 @@ MaterialStorage::Samplers MaterialStorage::samplers_rd_allocate(float p_mipmap_b
 					sampler_state.min_filter = RD::SAMPLER_FILTER_LINEAR;
 					sampler_state.mip_filter = mip_filter;
 					sampler_state.lod_bias = samplers.mipmap_bias;
-					sampler_state.use_anisotropy = anisotropy_max != 1;
+					sampler_state.use_anisotropy = anisotropy_max > (1.0 + CMP_EPSILON);
 					sampler_state.anisotropy_max = anisotropy_max;
 
 				} break;
